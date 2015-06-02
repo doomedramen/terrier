3687184f4046c42f2e046bafd2ab675913de38bc
Martin Page <wookoouk@gmail.com>
2015-06-02 10:37:31 +0100

    removed old deps, new menu + bar

--- js/terrier.js
+++ js/terrier.js
@@ -1,15 +1,34 @@
-function initSlideOut() {
-  var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70
-  });
-  document.querySelector('.menuButton').addEventListener('click', function () {
-    slideout.toggle();
-  });
+function initMenu() {
+
+  var body = $('body');
+  var panel = $('#panel');
+  var menuButton = $('.menuButton');
+
+  function onMenuButton() {
+    body.toggleClass('menuOpen');
+    setTimeout(function () {
+      panel.on('click', onPanel);
+      menuButton.off();
+    }, 3)
+  }
+
+  function onPanel() {
+    body.removeClass('menuOpen');
+    setTimeout(function () {
+      menuButton.on('click', onMenuButton);
+      panel.off();
+    }, 3)
+  }
+
+  if (menuButton.length && panel.length) {
+    //kick it off
+    menuButton.on('click', onMenuButton);
+  }
+
+
 }
 
+
 $(function () {
-  initSlideOut();
+  initMenu();
 });
