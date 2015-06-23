c6819c9a3a3f6ed2d118a03a70a25e96c73f5539
Martin Page <wookoouk@gmail.com>
2015-06-23 10:00:56 +0100

    removed all dependencies

--- js/terrier.js
+++ js/terrier.js
@@ -1,34 +1,65 @@
 function initMenu() {
 
-  var body = $('body');
-  var panel = $('#panel');
-  var menuButton = $('.menuButton');
+  var body = document.querySelectorAll('body')[0];
+  var panel = document.querySelectorAll('#panel')[0];
+  var menuButton = document.querySelectorAll('.menuButton')[0];
 
   function onMenuButton() {
-    body.toggleClass('menuOpen');
+    console.log('on menu');
+    toggleClass(body, 'menuOpen');
     setTimeout(function () {
-      panel.on('click', onPanel);
-      menuButton.off();
-    }, 3)
+      menuButton.removeEventListener('click', onMenuButton, false);
+      panel.addEventListener('click', onPanel);
+    }, 10)
   }
 
   function onPanel() {
-    body.removeClass('menuOpen');
+    console.log('on panel');
+    removeClass(body, 'menuOpen');
     setTimeout(function () {
-      menuButton.on('click', onMenuButton);
-      panel.off();
-    }, 3)
+      panel.removeEventListener('click', onPanel, false);
+      menuButton.addEventListener('click', onMenuButton);
+    }, 10)
   }
 
-  if (menuButton.length && panel.length) {
+  if (menuButton && panel) {
     //kick it off
-    menuButton.on('click', onMenuButton);
+    console.log(menuButton, panel);
+    menuButton.addEventListener('click', onMenuButton);
   }
-
 
 }
 
+function toggleClass(el, className) {
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
 
-$(function () {
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+
+    el.className = classes.join(' ');
+  }
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
+
+
+if (document.readyState != 'loading') {
+  init();
+} else {
+  document.addEventListener('DOMContentLoaded', init);
+}
+
+function init() {
   initMenu();
-});
+}
